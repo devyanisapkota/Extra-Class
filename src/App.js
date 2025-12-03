03b0f1b89385a33da7082bb044973908211d9dc7
Devyani Sapkota <devyanisapkota@gmail.com>
2025-12-03 09:02:18 +0545

    React props

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import Header from "./Header";
 import Footer from "./Footer";
 import List from "./List";
 import Card from "./Card";
+import Student from "./Student";
 function App() {
   return (
    <>
    <h1>Welcome to React Class</h1>
    <Header/>
    <List/>
+    <Student name="Devyani" age={20} isStudent="true"/>
+    <Card/>
   <Footer/>
-  <Card/>
+  
+  
    </>
   );
 }
---
+++ src/Student.jsx
@@ -0,0 +1,11 @@
+function Student({name="Guest",age="20",isStudent=false}){
+    return(
+        <>
+        <p>Nmae:{name}</p>
+        <p>Age:{age}</p>
+        <p>Student:{isStudent}</p>
+        </>
+    )
+
+}
+export default Student
