226ea453e00f06ef350d10b47697f1efad3afa2a
Devyani Sapkota <devyanisapkota@gmail.com>
2025-12-08 08:49:15 +0545

    Counter app added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Footer from "./Footer";
 import List from "./List";
 import Card from "./Card";
 import Student from "./Student";
+import Counter from "./Counter";
 function App() {
   return (
    <>
    <h1>Welcome to React Class</h1>
    <Header/>
    <List/>
+   <Counter/>
     <Student name="Devyani" age={20} isStudent="true"/>
     <Card/>
   <Footer/>
---
+++ src/Counter.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+function Counter() {
+    const [count, setCount] =useState(0)
+        function increase() {
+            setCount(count+1)
+
+        }
+
+        function decrease() {
+            setCount(count-1)
+
+        }
+
+        function reset() {
+            setCount(0)
+
+        }
+    return (
+        <>
+            <h1>Use State Hook Example</h1>
+            <p>You have clicked {count} Times</p>
+            <button onClick={increase}>Increase</button>
+            <button onClick={decrease}>Decrease</button>
+            <button onClick={reset}>Reset</button>
+
+        </>
+    )
+}
+
+
+export default Counter
