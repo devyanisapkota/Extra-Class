73e8f1e0a4f81a8e1bf01ae57e6daf1a0888ad02
Devyani Sapkota <devyanisapkota@gmail.com>
2025-12-17 09:03:40 +0545

    Login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
+import Login from "./Login";
 
 function App() {
   return (
    <>
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1> 
-      <div className="bg-red-600 m-10 p-2 cursor-pointer h-25 w-25 hover:bg-red-950 text-2xl text-white">
-        Example
-         </div>  
-        <button className="bg-blue-500 p-2 rounded-md text-white text-2xl transition-all duration-500 hover:bg-blue-950">
-          Click Me
-        </button>
-   </>
+   <Login/>
+    </>
   );
 }
 
---
+++ src/Login.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className="bg-gradient-to-r from-blue-400 to-purple-500 h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg">
+        <h2 className="text-3xl font-bold text-center" >Login</h2>
+        <div className="mb-6">
+        <label className="text-lg font-semibold">Username</label>
+       <input type="text" placeholder="Enter Your username" className="border border-gray-300 w-full p-3 rounded-lg"></input>
+        </div>
+        <div className="mb-6">
+       <label className="text-lg font-semibold">Password</label>
+       <input type="password" placeholder="Enter Your password" className="border border-gray-300 w-full p-3 rounded-lg"></input>
+        </div>
+        <div className="bg-blue-600 text-white text-center w-full p-3 rounded-lg font-semibold hover:bg-blue-700 ">
+            <input type="submit"value="Log In" ></input>
+           
+            </div>
+             <div>
+             <p className="text-center text-sm mt-4 text-gray-600"> Don't have an account?{""}
+             </p>
+             <div>
+              <p className="text-blue-600 cursor-pointer hover:underline text-center"> Sign up {""}</p>
+             </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
